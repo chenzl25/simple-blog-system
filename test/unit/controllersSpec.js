275cc8cfab0d8ffff723e467603cb59302634cc8
Dylan <chenzl25@mail2.sysu.edu.cn>
2016-01-24 12:38:08 +0800

    some config

--- test/unit/controllersSpec.js
+++ test/unit/controllersSpec.js
@@ -14,59 +14,59 @@ describe('login controllers', function() {
   beforeEach(module('blogApp'));
   beforeEach(module('blogServices'));
 
-  describe('PhoneListCtrl', function(){
-    var scope, ctrl, $httpBackend;
+  // describe('PhoneListCtrl', function(){
+  //   var scope, ctrl, $httpBackend;
 
-    beforeEach(inject(function(_$httpBackend_, $rootScope, $controller) {
-      $httpBackend = _$httpBackend_;
-      $httpBackend.expectGET('phones/phones.json').
-          respond([{name: 'Nexus S'}, {name: 'Motorola DROID'}]);
+  //   beforeEach(inject(function(_$httpBackend_, $rootScope, $controller) {
+  //     $httpBackend = _$httpBackend_;
+  //     $httpBackend.expectGET('phones/phones.json').
+  //         respond([{name: 'Nexus S'}, {name: 'Motorola DROID'}]);
 
-      scope = $rootScope.$new();
-      ctrl = $controller('PhoneListCtrl', {$scope: scope});
-    }));
-
-
-    it('should create "phones" model with 2 phones fetched from xhr', function() {
-      expect(scope.phones).toEqualData([]);
-      $httpBackend.flush();
-
-      expect(scope.phones).toEqualData(
-          [{name: 'Nexus S'}, {name: 'Motorola DROID'}]);
-    });
+  //     scope = $rootScope.$new();
+  //     ctrl = $controller('PhoneListCtrl', {$scope: scope});
+  //   }));
 
 
-    it('should set the default value of orderProp model', function() {
-      expect(scope.orderProp).toBe('age');
-    });
-  });
+  //   it('should create "phones" model with 2 phones fetched from xhr', function() {
+  //     expect(scope.phones).toEqualData([]);
+  //     $httpBackend.flush();
+
+  //     expect(scope.phones).toEqualData(
+  //         [{name: 'Nexus S'}, {name: 'Motorola DROID'}]);
+  //   });
 
 
-  describe('PhoneDetailCtrl', function(){
-    var scope, $httpBackend, ctrl,
-        xyzPhoneData = function() {
-          return {
-            name: 'phone xyz',
-                images: ['image/url1.png', 'image/url2.png']
-          }
-        };
+  //   it('should set the default value of orderProp model', function() {
+  //     expect(scope.orderProp).toBe('age');
+  //   });
+  // });
 
 
-    beforeEach(inject(function(_$httpBackend_, $rootScope, $routeParams, $controller) {
-      $httpBackend = _$httpBackend_;
-      $httpBackend.expectGET('phones/xyz.json').respond(xyzPhoneData());
+  // describe('PhoneDetailCtrl', function(){
+  //   var scope, $httpBackend, ctrl,
+  //       xyzPhoneData = function() {
+  //         return {
+  //           name: 'phone xyz',
+  //               images: ['image/url1.png', 'image/url2.png']
+  //         }
+  //       };
 
-      $routeParams.phoneId = 'xyz';
-      scope = $rootScope.$new();
-      ctrl = $controller('PhoneDetailCtrl', {$scope: scope});
-    }));
+
+  //   beforeEach(inject(function(_$httpBackend_, $rootScope, $routeParams, $controller) {
+  //     $httpBackend = _$httpBackend_;
+  //     $httpBackend.expectGET('phones/xyz.json').respond(xyzPhoneData());
+
+  //     $routeParams.phoneId = 'xyz';
+  //     scope = $rootScope.$new();
+  //     ctrl = $controller('PhoneDetailCtrl', {$scope: scope});
+  //   }));
 
 
-    it('should fetch phone detail', function() {
-      expect(scope.phone).toEqualData({});
-      $httpBackend.flush();
+  //   it('should fetch phone detail', function() {
+  //     expect(scope.phone).toEqualData({});
+  //     $httpBackend.flush();
 
-      expect(scope.phone).toEqualData(xyzPhoneData());
-    });
-  });
+  //     expect(scope.phone).toEqualData(xyzPhoneData());
+  //   });
+  // });
 });
